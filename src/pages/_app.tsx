8fbdec76fe972175e888a369316be6fbdab3aad1
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2022-09-22 15:37:35 -0400

    add: rainbowkit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,65 @@
 import { ChakraProvider } from '@chakra-ui/react'
-
+import '../styles/global.css';
 import theme from '../theme'
-import { AppProps } from 'next/app'
+import '@rainbow-me/rainbowkit/styles.css';
+import type { AppProps } from 'next/app';
+import {
+  RainbowKitProvider,
+  getDefaultWallets,
+  connectorsForWallets,
+  wallet,
+} from '@rainbow-me/rainbowkit';
+import { chain, createClient, configureChains, WagmiConfig } from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [
+    chain.polygonMumbai,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true'
+      ? [chain.polygonMumbai]
+      : []),
+  ],
+  [
+    alchemyProvider({ apiKey: '_gg7wSSi0KMBsdKnGVfHDueq6xMB9EkC' }),
+    publicProvider(),
+  ]
+);
+
+const { wallets } = getDefaultWallets({
+  appName: 'Regen Domain Service',
+  chains,
+});
+
+const demoAppInfo = {
+  appName: 'Regen Domain Service',
+};
+
+const connectors = connectorsForWallets([
+  ...wallets,
+  {
+    groupName: 'Other',
+    wallets: [wallet.argent({ chains }), wallet.trust({ chains })],
+  },
+]);
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+  webSocketProvider,
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  )
+    <WagmiConfig client={wagmiClient}>
+      <ChakraProvider theme={theme}>
+        <RainbowKitProvider appInfo={demoAppInfo} chains={chains}>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </ChakraProvider>
+    </WagmiConfig>
+  );
 }
 
-export default MyApp
+export default MyApp;
