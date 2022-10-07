e6c0b7a2a4873392f72b81728955c4570ff91f24
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2022-10-07 13:26:44 -0400

    perf(useDomains): add callbacks

--- src/hooks/useDomains.ts
+++ src/hooks/useDomains.ts
@@ -1,9 +1,9 @@
 import { ethers } from "ethers";
 import { useState, useEffect, useCallback } from 'react';
-import { CONTRACT_ADDRESS } from "../utils/constants";
+import { CONTRACT_ADDRESS } from '../utils/constants';
 import contractAbi from "../utils/contract-abi.json";
 import { Domain } from "../utils/types";
-
+import { useAccount, useSigner } from 'wagmi';
 /**
    * Fetch all domain names from contract, the owner's addresses for each domain,
    * and the record for each domain retrieved.
@@ -11,54 +11,90 @@ import { Domain } from "../utils/types";
    */
 export default function useDomains() {
   const [domains, setDomains] = useState<Domain[]>([])
-
+  const { data: signer } = useSigner()
+  const { isConnected, isReconnecting, address } = useAccount()
   // wrap refetchDomains in a usecallback to prevent re-renders
-  const refetchDomains = useCallback(async () => {
+  const fetchDomains = useCallback(async () => {
     try {
-      const { ethereum } = window;
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
+      if (signer) {
         const contract = new ethers.Contract(CONTRACT_ADDRESS, contractAbi.abi, signer);
-        console.log('getting all names')
 
         // Get all the domain names from our contract
         const names = await contract.getAllNames();
 
         // For each name, get the record and the address
-        const domainRecords: Domain[] = await Promise.all(
+        const domainRecords = await Promise.all(
           names.map(async (name: string) => {
             const mintRecord = await contract.records(name);
             const owner = await contract.domains(name);
-            return {
+
+            //Construct the domain
+            const domain = {
               id: names.indexOf(name),
               name: name,
               record: mintRecord,
               owner: owner,
-            }
+            };
+            return domain
           })
         );
 
-        console.log("MINTS FETCHED ", domainRecords);
-
-        setDomains([...domainRecords])
-        return domainRecords;
+        console.log('Mints fetched', domainRecords)
+        setDomains(domainRecords)
       }
+
     } catch (error) {
-      console.log(error);
+      console.error('error message: ', error);
     }
-  }, [])
+  }, [signer])
+
+
+  /**
+ * Fetch all domain names from contract, the owner's addresses for each domain,
+ * and the record for each domain retrieved.
+ * Put them into an array and set array as mints
+ */
+  // const fetchDomains = async () => {
+  //   try {
+  //     const { ethereum } = window;
+  //     if (ethereum && signer) {
+  //       // You know all this
+  //       const provider = new ethers.providers.Web3Provider(ethereum);
+  //       const signer = provider.getSigner();
+  //       const contract = new ethers.Contract(CONTRACT_ADDRESS, contractAbi.abi, signer);
+
+  //       // Get all the domain names from our contract
+  //       const names = await contract.getAllNames();
+
+  //       // For each name, get the record and the address
+  //       const mintRecords = await Promise.all(
+  //         names.map(async (name) => {
+  //           const mintRecord = await contract.records(name);
+  //           const owner = await contract.domains(name);
+  //           return {
+  //             id: names.indexOf(name),
+  //             name: name,
+  //             record: mintRecord,
+  //             owner: owner,
+  //           };
+  //         })
+  //       );
+
+  //       console.log("MINTS FETCHED ", mintRecords);
+  //       setDomains(mintRecords);
+  //     }
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
 
   useEffect(() => {
-    console.log('use effect')
-    refetchDomains()
-    // fetchDomains().then(resp => setDomains([...resp]))
-  }, [refetchDomains])
-
+    fetchDomains()
+  }, [signer])
 
   return {
     domains,
     totalDomains: domains.length,
-    refetchDomains
+    fetchDomains
   }
 }
