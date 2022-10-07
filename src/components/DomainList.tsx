f414527a851389bf20e7343050f623b4664129e9
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2022-10-07 13:26:16 -0400

    chore: remove hook from list

--- src/components/DomainList.tsx
+++ src/components/DomainList.tsx
@@ -1,11 +1,8 @@
 import { Flex, Stack, Text } from '@chakra-ui/react'
 import { memo } from 'react'
-import useDomains from '../hooks/useDomains'
 import DomainCard from './DomainCard'
 
-const DomainList = () => {
-  const { domains } = useDomains()
-
+const DomainList = ({ domains }) => {
   return (
     <>
       <Stack my='20' justifyContent='center' alignItems='center'>
@@ -13,9 +10,7 @@ const DomainList = () => {
           Recently minted domains
         </Text>
         <Flex flexWrap='wrap' justifyContent='center' mx='auto'>
-          {domains.map((domain) => (
-            <DomainCard domain={domain} key={domain.id} />
-          ))}
+          {domains && domains.map((domain) => <DomainCard domain={domain} key={domain.id} />)}
         </Flex>
       </Stack>
     </>
